ec7c80c1421f08ffe4ebe47487cfc24fd2713d9a
msuarz <mikemps@gmail.com>
2025-04-22 18:30:55 -0400

    Update .gitignore to exclude node_modules and docs

--- dist/server.js
+++ dist/server.js
@@ -6,19 +6,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const http_1 = require("http");
 const socket_io_1 = require("socket.io");
+const { McpServer } = require('@modelcontextprotocol/sdk');
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
 const httpServer = (0, http_1.createServer)(app);
-const io = new socket_io_1.Server(httpServer);
-const mcpResponse = {
-    jsonrpc: '2.0',
-    result: { instructions: [{ content: [{ type: 'text', text: 'now i know tcr' }] }] }
-};
-app.post('/mcp', express_1.default.json(), (req, res) => {
-    res.json({ ...mcpResponse, id: req.body.id });
+const io = new socket_io_1.Server(httpServer, { cors: { origin: '*' } });
+io.on('connection', socket => socket.on('tcr:instructions', () => socket.emit('tcr:instructions:response', { message: 'now i know tcr' })));
+app.get('/', (_, res) => res.send('mcvibes'));
+const mcpServer = new McpServer({
+    name: 'mcvibes',
+    version: '0.2.0'
 });
-io.on('connection', socket => {
-    socket.on('tcr:instructions', () => {
-        socket.emit('tcr:instructions:response', { message: 'now i know tcr' });
-    });
+mcpServer.setToolHandler('run_tcr', {}, async () => {
+    return {
+        content: [{ type: 'text', text: 'now i know tcr' }]
+    };
 });
-httpServer.listen(3000);
+mcpServer.listen(httpServer, { path: '/mcp' });
+httpServer.listen(3000, () => console.error('mcvibes server running on port 3000'));
