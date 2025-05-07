2ec7e72bc844b4150f3db7124cc60ff7dfe5f80f
msuarz <mikemps@gmail.com>
2025-05-07 00:11:48 -0400

    clean:remove console output

--- src/vibes.js
+++ src/vibes.js
@@ -35,8 +35,6 @@ server.setRequestHandler(
 async function runServer() {
   const transport = new StdioServerTransport();
   await server.connect(transport);
-  console.error('MC Vibes in da houze');
 }
 
-console.error('MC Vibes starting...');
 runServer();
