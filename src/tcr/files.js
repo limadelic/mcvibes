710cb724566ddb39b112333bdb042bda49d0e74e
msuarz <mikemps@gmail.com>
2025-05-22 23:55:01 -0400

    simplify:file status

--- src/tcr/files.js
+++ src/tcr/files.js
@@ -4,23 +4,15 @@ import {
   untracked,
 } from '../helpers/git.js';
 
-const section = (title, files) =>
-  files.length > 0
-    ? `\n${title}:\n\n${files.join('\n')}\n\n`
-    : '';
-
-const list = () =>
-  section('Files changed', [
-    ...changed(),
-    ...staged(),
-  ]);
-
-const added = () =>
-  section('New files', untracked());
-
 export const total = () =>
   changed().length +
   staged().length +
   untracked().length;
 
-export const status = () => list() + added();
+export const status = () => `
+Files changed:
+
+${changed().join('\n')}
+${staged().join('\n')}
+${untracked().join(' (*)\n')}
+`;
